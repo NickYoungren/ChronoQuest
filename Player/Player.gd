fe8cdd3eeb050401893de29d83bce1f4caaf13e9
policarddavid <policarddavid@ufl.edu>
2024-03-06 19:11:17 -0500

    added shooting mechanics

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,12 +5,15 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 var rewp = position
 var b
+var bul
+var flipped = 1
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var rew = false
 @onready var anim = get_node("AnimationPlayer")
 
 @export var orb : PackedScene
+@export var bullet : PackedScene
 
 
 func rewind():
@@ -23,11 +26,25 @@ func rewind():
 		position = rewp
 		owner.remove_child(b)
 	rew = !rew;
+	
+func shoot():
+	#add animation?
+	#anim.play("Shoot")
+	bul = bullet.instantiate()
+	bul.setDir(flipped)
+	owner.add_child(bul)
+	if flipped == -1:
+		bul.position = position + Vector2(-100, 0)
+	else:
+		bul.position = position
 
 func _physics_process(delta):
 	orb = load("res://scenes/orb.tscn")
+	bullet = load("res://scenes/bullet.tscn")
 	if Input.is_action_just_pressed("rewind"):
 		rewind()
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -42,8 +59,10 @@ func _physics_process(delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction == -1:
 		get_node("AnimatedSprite2D").flip_h = true
+		flipped = -1
 	elif direction == 1:
 		get_node("AnimatedSprite2D").flip_h = false
+		flipped = 1
 	if direction:
 		velocity.x = direction * SPEED
 		if velocity.y == 0:
--- Player/gun.gd
+++ Player/gun.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends Marker2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,5 +9,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
